90c079ed333dee6dc6fb54f9d76aaf3a992b3fd4
prinitha2021 <69192380+prinitha2021@users.noreply.github.com>
2022-02-10 13:38:27 +0530

    Create ascii_value.c
    To find Ascii value of a given character

---
+++ ascii_value.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Ascii value of a given character
+
+int main()
+{
+    char c;
+    printf("\nenter any character to find its Ascii value.");
+    scanf("%c",&c);
+    printf("\nThe Ascii value of %c is %d.",c,c);
+    
+    return 0;
+}
